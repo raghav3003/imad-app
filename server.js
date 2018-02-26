8c34de83322d1d41281c119bdafe950f6d909c15
raghav3003 <34268524+raghav3003@users.noreply.github.com>
2018-02-27 01:55:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send('Article one requested and will be served here');
 });
 
-app.get('/', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send('Article two requested and will be served here');
 });
 
-app.get('/', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send('Article three requested and will be served here');
 });
 
