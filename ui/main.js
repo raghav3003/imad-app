ced2d90c18670e973a393b402c37462363400b39
raghav3003 <34268524+raghav3003@users.noreply.github.com>
2018-02-28 15:21:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,9 @@
-console.log('Loaded!');
+var button = document.getElementById("counter");
 
-var img = document.getElementById('madi');
+var counter = 0;
 
-var marginLeft = 0;
-
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
+button.onclick = function() {
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
 }
-
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-};
-
